bdbbce6303f57a23c52776ef3935ea3ec8b41d24
Inchara B S <incharabs23@gmail.com>
2026-06-17 08:43:23 +0530

    Add files via upload
    single inheritance

---
+++ Citizen.java
@@ -0,0 +1,34 @@
+package com.tns.singleinheritance;
+
+public class Citizen {
+
+    private String name;
+    private String aadharNo;
+    private String address;
+    private long phno;
+
+    public Citizen(String name, String aadharNo,
+                   String address, long phno) {
+
+        this.name = name;
+        this.aadharNo = aadharNo;
+        this.address = address;
+        this.phno = phno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAadharNo() {
+        return aadharNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public long getPhno() {
+        return phno;
+    }
+}
---
+++ SLInheritance.java
@@ -0,0 +1,16 @@
+package com.tns.singleinheritance;
+
+public class SLInheritance {
+    public static void main(String[] args) {
+        Student obj = new Student(
+                "abhilash",
+                "1234LHGF",
+                "bengaluru",
+                123456L,
+                101,
+                "shridevi"
+        );
+
+        System.out.println(obj);
+    }
+}
